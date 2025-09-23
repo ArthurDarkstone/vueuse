fa93fc58931488efc1a0f103dbb1e0d40347f54a
Arthur Darkstone <arthurlli@qq.com>
2025-09-23 11:50:18 +0800

    feat(demo): refactor demo to use custom instance with useMounted

--- packages/core/useMounted/demo.vue
+++ packages/core/useMounted/demo.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
+import type { ComponentInternalInstance } from 'vue'
 import { useMounted } from '@vueuse/core'
-import { defineComponent, h, useTemplateRef } from 'vue'
+import { defineComponent, getCurrentInstance, h, shallowRef, useTemplateRef } from 'vue'
 
-const component = useTemplateRef('component')
+const component = useTemplateRef<typeof Comp1>('component')
 
-const isMounted = useMounted(component.value?.$)
+const customInstance = shallowRef<ComponentInternalInstance | null>(null)
 
 const Comp1 = defineComponent({
+
+  setup() {
+    const instance = getCurrentInstance()
+
+    customInstance.value = instance
+
+    return {}
+  },
+
   render() {
     return h('div', 'Component 1')
   },
 })
+
+const isMounted = useMounted(customInstance.value)
 </script>
 
 <template>

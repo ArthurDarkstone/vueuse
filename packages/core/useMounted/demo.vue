2f491b75d58a9fac90652952b87d6ed48623336f
Arthur Darkstone <arthurlli@qq.com>
2025-09-23 11:15:32 +0800

    feat(demo): update demo to use template ref and toggle mounted state

--- packages/core/useMounted/demo.vue
+++ packages/core/useMounted/demo.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import { useMounted } from '@vueuse/core'
+import { defineComponent, h, useTemplateRef } from 'vue'
 
-const isMounted = useMounted()
+const component = useTemplateRef('component')
+
+const isMounted = useMounted(component.value?.$)
+
+const Comp1 = defineComponent({
+  render() {
+    return h('div', 'Component 1')
+  },
+})
 </script>
 
 <template>
-  <div>{{ isMounted ? 'mounted' : 'unmounted' }}</div>
+  <Comp1 ref="component" />
+
+  <button @click="isMounted = !isMounted">
+    Toggle Mounted
+  </button>
 </template>
